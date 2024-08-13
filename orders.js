21a9c14a4e3ba51aab5437c92ad8bffe4646fd98
Ytbarek Bayu <154122367+ytbarek19@users.noreply.github.com>
2024-08-13 13:32:36 -0500

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Fashion Merchandising and Retailing',
+        productNumber: '85743',
+        paymentStatus: '4,797',
+        status: '186'
+    },
+    {
+        productName: 'Database Management Systems (DBMS)',
+        productNumber: '97245',
+        paymentStatus: '4,731',
+        status: '250'
+    },
+    {
+        productName: 'Kaizen for Service Industries',
+        productNumber: '36452',
+        paymentStatus: '4,958',
+        status: '23'
+    },
+]
